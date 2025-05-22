5cd95c0fb6c4d29e3ae8d66135cb7d4a450ba94c
Leandro Jeremias Cejas <Jeremias_785@hotmail.com>
2025-05-22 16:13:47 -0300

    Inicio del proyecto

---
+++ src/test/java/com/equipo1/fix_manager/FixManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo1.fix_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
